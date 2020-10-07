6e27085f0cb70eb07ccf06d979e67dbe000bf82c
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-10-07 14:15:45 +0530

    [pranav] Add. First Name Validation is Provided

---
+++ UserReg/Pattern.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserReg
+{
+    class Pattern
+    {
+        string firstNameReGex = "^[A-Z][a-z]{2,}$";
+
+        public void checkFirstName()
+        {
+            Console.WriteLine("Enter your First Name: ");
+            string firstName = Console.ReadLine();
+
+            if (Regex.IsMatch(firstName, firstNameReGex) == true)
+            {
+                Console.WriteLine("First Name " + firstName + " is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name " + firstName + " is not valid");
+            }
+        }
+    }
+}
---
+++ UserReg/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserReg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+            Pattern pattern = new Pattern();
+            pattern.checkFirstName();
+        }
+    }
+}
